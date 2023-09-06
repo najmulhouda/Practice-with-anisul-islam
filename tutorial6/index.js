8965354f80da33e59169c5d6ceb463e6108b8ad9
najmulhouda <najmulhouda6@gmail.com>
2023-09-07 01:13:03 +0600

    tutorial 7 complited

---
+++ tutorial6/index.js
@@ -0,0 +1,18 @@
+// var num = 50;
+
+// num = toString(num);
+
+// console.log(typeof(num));
+
+// var num = "50";
+// num = parseInt(num);
+// console.log(typeof(num));
+
+
+// var num = "5.5";
+// num = parseFloat(num);
+// console.log(typeof(num));
+
+var num = 2.34343;
+
+console.log(num.toFixed(1));
---
+++ tutorial7/index.js
@@ -0,0 +1,13 @@
+// var firstName = "Najmul";
+// var lastName = "Houda";
+
+// var fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+
+var num1 = 5;
+var num2 = 10;
+
+
+console.log("Num1 =" + num1 + " Num2 =" + num2);
