f4296d6879e09f5bb9551a2aa05963c809fe018a
najmulhouda <najmulhouda6@gmail.com>
2023-10-17 16:43:26 +0600

    user prefarence update

--- tutorial91/index.js
+++ tutorial91/index.js
@@ -1,14 +1,17 @@
 const selectFontSize = document.getElementById("selectFontSize");
 const selectBgColor = document.getElementById("selectBgColor");
+const selectFontFamily = document.getElementById("selectFontFamily");
 const resetButton = document.getElementById("resetButton");
 const mainElement = document.querySelector("main");
 
 
-const setValue = (fontSize , bgColor) =>{
+const setValue = (fontSize , bgColor , fontFamily) =>{
     selectFontSize.value = fontSize;
     selectBgColor.value = bgColor;
+    selectFontFamily.value = fontFamily;
     mainElement.style.fontSize = fontSize;
     mainElement.style.backgroundColor = bgColor;
+    mainElement.style.fontFamily = fontFamily;
 }
 
 
@@ -16,16 +19,20 @@ const setValue = (fontSize , bgColor) =>{
 const initialSetup = () =>{
     const fontSize = localStorage.getItem("fontSize");
     const bgColor = localStorage.getItem("bgColor");
-    if(fontSize && bgColor){
-        setValue(fontSize , bgColor);
-    }else if(!fontSize && !bgColor){
-        setValue("16px" , "aqua");
+    const fontFamily = localStorage.getItem("fontFamily");
+    if(fontSize && bgColor && fontFamily){
+        setValue(fontSize , bgColor , fontFamily);
+    }else if(!fontSize && !bgColor && !fontFamily){
+        setValue("16px" , "aqua" , "'Noto Sans KR', sans-serif");
     }
-    else if(fontSize && !bgColor){
-        setValue(fontSize , "aqua");
+    else if(fontSize && !bgColor && !fontFamily){
+        setValue(fontSize , "aqua" , "'Noto Sans KR', sans-serif");
     }
-    else if(!fontSize && bgColor){
-        setValue("16px" , bgColor);
+    else if(!fontSize && bgColor    && !fontFamily){
+        setValue("16px" , bgColor    , "'Noto Sans KR', sans-serif");
+    }
+    else if(!fontSize && !bgColor    && fontFamily){
+        setValue("16px" , "aqua"    , fontFamily);
     }
 }
 
@@ -43,17 +50,24 @@ const changeBgColor = (event) => {
     mainElement.style.backgroundColor = selectedBgColor;
     localStorage.setItem("bgColor", selectedBgColor);
 }
-
+const changeFontFamily = (event) => {
+    const selectedFontFamily = event.target.value;
+    mainElement.style.fontFamily = selectedFontFamily;
+    localStorage.setItem("fontFamily", selectedFontFamily);
+}
 
 selectFontSize.addEventListener("change", changeFontSize);
 selectBgColor.addEventListener("change", changeBgColor);
+selectFontFamily.addEventListener("change", changeFontFamily);
 
 resetButton.addEventListener("click", () => {
     mainElement.style.fontSize = "16px";
     mainElement.style.backgroundColor = "aqua";
+    mainElement.style.fontFamily = "Alumni sans Collegiote One";
     localStorage.removeItem("fontSize");
     localStorage.removeItem("bgColor");  
-    setValue("16px" , "aqua ")
+    localStorage.removeItem("fontFamily");  
+    setValue("16px" , "aqua" , "Alumni sans Collegiote One");
 })
 
 initialSetup();
